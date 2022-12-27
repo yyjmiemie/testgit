8513f658eb952bb3e38ad2916cb61cd93c43dc4c
Yujie Yang <yujie.yang@agile-robots.com>
2022-12-27 09:47:50 +0000

    Update main.cpp-modify cout

--- main.cpp
+++ main.cpp
@@ -3,4 +3,5 @@
 int main(int, char**) {
     std::cout << "Hello, world!\n";
     std::cout<<"this is the program for testing git and gitlab and github";
+    std::cout<<"modify at git.cn.ar.int";
 }
